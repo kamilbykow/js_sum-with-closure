dc93de1c0217cf2efcd6be39226ff55fe6343aa6
Kamil <kamilbykowski99@gmail.com>
2024-09-05 13:58:44 +0200

    Directly returned functions

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -8,17 +8,13 @@
 function makeRobotAccountant() {
   let countCalls = 0;
 
-  function sum(a) {
-    function add(b) {
+  return (a) => {
+    return (b) => {
       countCalls++;
 
       return countCalls % 2 === 0 && countCalls > 3 ? 'Bzzz... Error!' : a + b;
-    }
-
-    return add;
-  }
-
-  return sum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
