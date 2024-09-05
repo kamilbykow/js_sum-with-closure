131cca0587899efffae86a108ffebcc4a032575f
Kamil <kamilbykowski99@gmail.com>
2024-09-05 11:53:21 +0200

    Sum with a cloasure

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let countCalls = 0;
+
+  function sum(a) {
+    function add(b) {
+      countCalls++;
+
+      return countCalls % 2 === 0 && countCalls > 3 ? 'Bzzz... Error!' : a + b;
+    }
+
+    return add;
+  }
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
